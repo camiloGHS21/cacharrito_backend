c1ae90b3ec69ec55758b8d6e69860931f7752239
camiloghs21 <136854384+camiloGHS21@users.noreply.github.com>
2024-04-07 10:10:30 -0700

    primer commit del proyecto

---
+++ src/main/java/com/example/cachorrito/CacharritoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.cachorrito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CacharritoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CacharritoBackendApplication.class, args);
+	}
+
+}
