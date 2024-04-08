eb0907808292f46f411357c9775b9c086dd5b90e
camiloghs21 <136854384+camiloGHS21@users.noreply.github.com>
2024-04-08 12:50:55 -0700

    creando backend de Automoviles modelo, controlador, repositorio

--- src/main/java/com/example/cacharrito/controlador/ControladorAutomoviles.java
+++ src/main/java/com/example/cacharrito/controlador/ControladorAutomoviles.java
@@ -1,5 +1,60 @@
 package com.example.cacharrito.controlador;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import com.example.cacharrito.modelo.Automoviles;
+import com.example.cacharrito.repositorio.RepositorioAutomoviles ;
+
+@RestController
+@RequestMapping("/automoviles")
 public class ControladorAutomoviles {
 	
+	@Autowired
+	  private RepositorioAutomoviles repositorio;
+	
+	@GetMapping("/verTodos")
+	public List<Automoviles> verTodosAutomoviles(){
+		return repositorio.findAll();
+	}
+	  
+	 @GetMapping("/buscarId")
+	  public Optional<Automoviles> verAutomovil(@RequestBody Automoviles automovil) {
+		  return repositorio.findById(automovil.getNumero_de_automovil());
+		  
+	  }
+	  
+	  
+	  @PostMapping("/guardar")
+	  public Automoviles guardarAutomovil(@RequestBody Automoviles automovil ) {
+		  return repositorio.save(automovil);
+		  
+	  }
+	  
+	  @DeleteMapping("/borrar")
+	  public  ResponseEntity<String> borrarAutomovil(@RequestBody Automoviles automovil) {
+	      try {
+	          repositorio.deleteById(automovil.getNumero_de_automovil());
+	          return ResponseEntity.ok("Automovil eliminada correctamente");
+	      } catch (Exception e) {
+	          return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error al eliminar la Automovil: " + e.getMessage());
+	      }
+	  }
+    
+	   @PatchMapping("/actualizar")
+	   public ResponseEntity<String> actualizarAutomovil(@RequestBody Automoviles automovil){
+		   try {
+		          repositorio.save(automovil);
+		          return ResponseEntity.ok("Reservación actualizada correctamente");
+		      } catch (Exception e) {
+		          return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error al actualizar la Automovil: " + e.getMessage());
+		      }
+		   
+	   }
+	   
 }
--- src/main/java/com/example/cacharrito/repositorio/RepositorioAutomoviles.java
+++ src/main/java/com/example/cacharrito/repositorio/RepositorioAutomoviles.java
@@ -1,5 +1,11 @@
 package com.example.cacharrito.repositorio;
 
-public interface RepositorioAutomoviles {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.cacharrito.modelo.Automoviles;
+
+@Repository
+public interface RepositorioAutomoviles  extends JpaRepository<Automoviles,Long>{
  
 }
