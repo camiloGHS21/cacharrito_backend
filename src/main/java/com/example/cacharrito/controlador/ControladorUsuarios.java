7595f3bcb90a66461a6448e587195d4b92182df0
camiloghs21 <136854384+camiloGHS21@users.noreply.github.com>
2024-04-08 12:31:07 -0700

    cambiando nombre de url ControladorUsuarios.java

--- src/main/java/com/example/cacharrito/controlador/ControladorUsuarios.java
+++ src/main/java/com/example/cacharrito/controlador/ControladorUsuarios.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/api/usuarios")
+@RequestMapping("/usuarios")
 public class ControladorUsuarios {
 
 	@Autowired
